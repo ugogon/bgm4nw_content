76bad93ee7b916251f43cb2ee5e4bff70b89b1aa
Felix <felix.leitner@med.uni-goettingen.de>
2026-01-05 14:59:23 +0100

    update(file): scripts/beispiel_skript.r

--- 3_scripts/beispiel_skript.r
+++ 3_scripts/beispiel_skript.r
@@ -6,12 +6,16 @@ dat <- read.csv("input.csv")
 
 # Berechnungen
 Umgebung <- mean(dat$Umgebung1, dat$Umgebung2)
-
-#Text <- ifelse(Umgebung <= 2)
+Umgebung <- Umgebung + 1
 
 Text1 <- "Ihre Arbeitsumgebung ist wenig digitalisiert."
 Text2 <- "Ihre Arbeitsumgebung ist mittelmäßig digitalisiert."
 Text3 <- "Ihre Arbeitsumgebung ist stark digitalisiert."
+
+Text <- Text1
+Text <- ifelse(Umgebung == 3, Text2, Text)
+Text <- ifelse(Umgebung > 3, Text3, Text)
+
 
 # Ergebnisse
 result <- data.frame(Umgebung = Umgebung)
