2b282820d0e523e8a6247f031defe6bc7d1dd52e
Felix <felix.leitner@med.uni-goettingen.de>
2026-01-05 14:30:37 +0100

    update(file): scripts/beispiel_skript.r

--- 3_scripts/beispiel_skript.r
+++ 3_scripts/beispiel_skript.r
@@ -1,6 +1,6 @@
 # Der allgemeine Dataframe mit allen Variablen
 dat <- read.csv("input.csv")
-
-result <- data.frame()
+Umgebung <- mean(Umgebung1, Umgebung2)
+result <- data.frame(Umgebung <- Umgebung)
 exportJSON <- toJSON(result)
 write(exportJSON, "output.json")
