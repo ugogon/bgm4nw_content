e9508db4cb448d97a8d5a3f7e4f205c54c5beee9
Felix <felix.leitner@med.uni-goettingen.de>
2026-01-05 16:05:14 +0100

    create(file): scripts/test.r

---
+++ 3_scripts/test.r
@@ -0,0 +1,8 @@
+---------------------------------------------------------------------------
+  # Packages ####
+#install.packages("tidyverse")
+install.packages("dendextend")
+#install.packages("factoextra")
+
+library(tidyverse)
+library(cluster)
