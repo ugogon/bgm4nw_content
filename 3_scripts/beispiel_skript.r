bd56ac8373f78277bf05ba54497833e9f3cc0662
Felix <felix.leitner@med.uni-goettingen.de>
2026-01-07 09:26:57 +0100

    update(file): scripts/beispiel_skript.r

--- 3_scripts/beispiel_skript.r
+++ 3_scripts/beispiel_skript.r
@@ -29,12 +29,17 @@ RefVal2 <- ifelse(Umgebung > 3, 4, RefVal2)
 
 Problem <- ifelse(CompVal1 < RefVal1, 1, 0)
 
+CompText1 <- "Ihre digitale Kompetenz ist im Vergleich zu anderen Personen, welche in ähnlich digitalen Arbeitssettings angestellt sind, eher niedrig ausgeprägt. Hier könnte Handlungsbedarf bestehen. Informieren Sie sich am besten über Angebote und Materialien, welche Ihnen helfen können, diese zu stärken."
+CompText2 <- "Ihre digitale Kompetenz ist im Vergleich zu anderen Personen, welche in ähnlich digitalen Arbeitssettings angestellt sind, gut ausgeprägt. Hier besteht aktuell kein Handlungsbedarf."
+CompText <- ifelse(Problem == 1, CompText1, CompText2)
+
 # Ergebnisse
 result <- data.frame(Umgebung = Umgebung,
                      Text = Text,
                      CompVal1 = CompVal1,
                      CompVal2 = CompVal2,
                      RefVal1 = RefVal1,
-                     RefVal2 = RefVal2)
+                     RefVal2 = RefVal2,
+                     CompText = CompText)
 exportJSON <- toJSON(result)
 write(exportJSON, "output.json")
